8dc5226830099992a98ca9c7974dee73739c58a3
Eric Schulte <schulte.eric@gmail.com>
2013-12-31 16:00:23 -0700

    added commentary

--- web-server.el
+++ web-server.el
@@ -6,6 +6,25 @@
 ;; Keywords: http
 ;; License: GPLV3 (see the COPYING file in this directory)
 
+;;; Commentary:
+
+;; A web server in Emacs running handlers written in Emacs Lisp.
+;;
+;; Full support for GET and POST requests including URL-encoded
+;; parameters and multi-part/form data.
+;;
+;; See the examples/ directory for examples demonstrating the usage of
+;; the Emacs Web Server.  The following launches a simple "hello
+;; world" server.
+;;
+;;     (ws-start
+;;      '(((lambda (_) t) .                         ; match every request
+;;         (lambda (request)                        ; reply with "hello world"
+;;           (with-slots (process) request
+;;             (ws-response-header process 200 '("Content-type" . "text/plain"))
+;;             (process-send-string process "hello world")))))
+;;      9000)
+
 ;;; Code:
 (require 'web-server-status-codes)
 (require 'mail-parse)             ; to parse multipart data in headers
