6e9f8168f25660609385b6436fa7de84f2bb032b
Eric Schulte <schulte.eric@gmail.com>
2014-01-07 09:26:42 -0700

    helpers for handling web socket connections

--- examples/9-web-socket.el
+++ examples/9-web-socket.el
@@ -1,11 +1,14 @@
 ;;; web-sockets.el --- communicate via web-sockets
 
-(defvar web-socket-page "<html>
+(defvar web-socket-port 8888)
+
+(defvar web-socket-page
+  (format "<html>
 <head>
 <script type=\"text/javascript\">
 var ws;
 function connect(){
-  ws = new WebSocket(\"ws://localhost:9999/\");
+  ws = new WebSocket(\"ws://localhost:%d/\");
 
   ws.onopen    = function()    { alert(\"connected\"); };
   ws.onmessage = function(msg) { alert(\"Server: \" + msg.data); };
@@ -22,14 +25,12 @@ function close(){ ws.close(); };
 <a href=\"javascript:message()\">message</a>
 <a href=\"javascript:close()\">close</a>
 </body>
-</html>")
+</html>" web-socket-port))
 
 (defun web-socket-server (request)
   (with-slots (process headers) request
-    (ws/web-socket-messages-do headers message
-      ;; (ws/web-socket-send message)
-      (message "GOT:%S" message))
+    (ws-web-socket-connect request 'ws-web-socket-send)
     (ws-response-header process 200 '("Content-type" . "text/html"))
     (process-send-string process web-socket-page)))
 
-(ws-start '(((:GET . ".*") . web-socket-server)) 9999)
+(ws-start '(((:GET . ".*") . web-socket-server)) web-socket-port)
